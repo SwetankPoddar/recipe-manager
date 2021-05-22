82bbae1eb2bf08f1c6ef5259a151f85054b8ae7f
Swetank Poddar <2424088p@student.gla.ac.uk>
2021-05-23 01:02:39 +0530

    [added] Intial version

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+var faunadb = require('faunadb')
+var client = new faunadb.Client({secret: process.env.FAUNADB_SECRET})
+
+const {
+  Index,
+  Function: Fn,
+  Call, 
+  Select,
+  Match,
+  Collection,
+  Ref,
+  Get,
+  Paginate
+} = faunadb.query
+
+export default async (req, res) => {
+  
+  const doc = await client.query(
+    Paginate(Match(Index('recipe_slugs')))
+  )
+
+  res.send(doc.data)
+
 }
