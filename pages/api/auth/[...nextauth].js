329c8f96c35a0451ffc00379ffd8b5aded79c3f6
Swetank Poddar <2424088p@student.gla.ac.uk>
2021-05-30 17:39:22 +0530

    [removed] unnecessory component page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
-import * as Fauna from "faunadb"
+// import * as Fauna from "faunadb"
 // import { FaunaAdapter } from "@next-auth/fauna-adapter"
 
 // const client = new Fauna.Client({secret: process.env.FAUNADB_SECRET})
